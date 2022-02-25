b01fbfbf87ab4fe355ad8ca965d219fe6d873ef6
Bada tomiwa <tomiwabd@gmail.com>
2022-02-24 20:54:50 -0800

    final touches

--- westernrise.js
+++ westernrise.js
@@ -1,25 +1,46 @@
     function openNav() {
     var x = document.getElementById("navlinks");
     var y = document.getElementById("main");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      y.style.marginLeft = "0";
-      y.style.marginRight = "0";
-    } else {
-      x.style.display = "block";
+    if (x.style.transform === "translateX(-80vw)") {
+      x.style.transform = "translateX(0)";
       y.style.marginLeft = "80vw";
       y.style.marginRight = "-80vw";
+    } else {
+      x.style.transform = "translateX(-80vw)";
+      y.style.marginLeft = "0";
+      y.style.marginRight = "0";
     }
   }
 
-    function openUp() {
-      var a = document.getElementById("checkout-tab")
-      a.style.display = "block";
+  /*window.onscroll = function(){scrollFunction()};
+
+  function scrollFunction() {
+    var a = document.getElementById("below-bar");
+    var b = document.getElementById("logo");
+    var c = document.getElementsByClassName("icon")
+    var d = document.getElementsByClassName("checkout")
+    if (document.body.scrollTop > 200 || documentElement.scrollTop > 200) {
+      a.style.backgroundColor = "red";
+    } else{
+      a.style.backgroundColor = "blue";
     }
+  }*/
     
-    function closeUp() {
-      var b = document.getElementById("checkout-tab")
-      b.style.display = "none";
-      var message = document.getElementById("name-text").value;
-      alert("Hey " + message  + ", Thank you for subscribing")
-    }
+
+ window.addEventListener('scroll', () => {
+    const a = document.getElementById("below-bar");
+    const b = document.getElementById("logo");
+    let c = document.getElementsByClassName("burger")[0];
+    let cc = document.getElementsByClassName("burger")[1];
+    let ccc = document.getElementsByClassName("burger")[2];
+    const d = document.getElementById("checkout");
+
+    a.style.backgroundColor = "white";
+    b.src="Mobile Assignment Images/logo-small.svg";
+    b.style.width = "40px";
+    d.style.backgroundColor = "#cbc4b6" ;
+    d.style.color ="white" ;
+    c.style.backgroundColor = "black" ;
+    cc.style.backgroundColor = "black" ;
+    ccc.style.backgroundColor = "black" ;
+  })
